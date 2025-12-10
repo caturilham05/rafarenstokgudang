4ffed47ea7103b7af99d293a923b88b1bf18f0b4
catur <catur@fisip.net>
2025-12-10 11:24:55 +0700

    shopee auth

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/shopee_redirect_auth_demo', function() {
+    $path = "/api/v2/shop/auth_partner";
+    $redirectUrl = "http://demo.rafarenstokgudang.com/";
+    $timest = time();
+    $baseString = sprintf("%s%s%s", env('SHOPEE_PARTNER_ID_TEST'), $path, $timest);
+    $sign = hash_hmac('sha256', $baseString, env('SHOPEE_PARTNER_KEY_TEST'));
+    $url = sprintf("%s%s?timestamp=%s&partner_id=%s&sign=%s&redirect=%s", env('SHOPEE_REDIRECT_URL_TEST'), $path, $timest, env('SHOPEE_PARTNER_ID_TEST'), $sign, $redirectUrl);
+    return $url;
+});
