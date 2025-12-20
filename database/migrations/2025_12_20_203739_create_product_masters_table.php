def89566c4fb8560dbc30c75a60b5734c97351a8
catur <catur@fisip.net>
2025-12-20 20:47:07 +0700

    feat: add product masters

---
+++ database/migrations/2025_12_20_203739_create_product_masters_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_masters', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('product_id')->unsigned()->comment('product id');
+            $table->foreign('product_id')->references('id')->on('products')->onUpdate('cascade')->onDelete('cascade');
+            $table->index('product_id');
+            $table->string('product_name');
+            $table->integer('stock')->nullable()->default(0)->unsigned();
+            $table->integer('stock_conversion')->nullable()->unsigned()->default(0)->comment('untuk menentukan berapa stock yang berkurang saat ada order');
+            $table->integer('sale')->nullable()->default(0)->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_masters');
+    }
+};
